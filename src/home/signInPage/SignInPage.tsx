a5d7da3ae5e3f081f3cd4b734be7074adf8197a3
SashaZaicev <sasha167@bk.ru>
2020-01-17 21:05:46 +0300

    add state

--- src/home/signInPage/SignInPage.tsx
+++ src/home/signInPage/SignInPage.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import {FORGOT_PASS, SIGN_UP} from "../Routes";
+import {connect} from "react-redux";
 
-const SignInPage = (props) => {
+const SignInPage: React.FC = (props: any) => {
     return (
         <div className='container'>
             <div>Введите свой логин и пароль!</div>
-            <div><input type="text" placeholder='Login'/></div>
-            <div><input type="password" placeholder='Password'/></div>
+            <div><input type="text" placeholder='Login' value={props.email}/></div>
+            <div><input type="password" placeholder='Password' value={props.password}/></div>
             <div><NavLink to={FORGOT_PASS}>Forgot password?</NavLink></div>
             <div><input type="checkbox"/>Remember me</div>
             <div><button>Sign in</button></div>
@@ -15,5 +16,11 @@ const SignInPage = (props) => {
         </div>
     );
 };
+const mstp = (state: any)=>{
+    return{
+        email: state.signIn.email,
+        password: state.signIn.password
+    }
+}
 
-export default SignInPage;
+export default connect (mstp)(SignInPage);
